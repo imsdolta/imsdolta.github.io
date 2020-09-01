0362385d2d27621512a40d9642aab3b19cbc7645
imsdolta <imsdolta@gmail.com>
2020-09-01 21:32:58 +0530

    :tada: testing

---
+++ index.js
@@ -0,0 +1,59 @@
+const formElem = document.getElementById('contact-form');
+const errorElem = document.getElementById('error');
+const API_URL = 'https://node-contact-app-server.herokuapp.com';
+
+
+formElem.addEventListener('submit', (event) => { // Grab the form data and make API req to save to db 
+    event.preventDefault();
+    const formData = new FormData(formElem);
+
+    var Name = formData.get('name');
+    var number = formData.get('number');
+    var Email = formData.get('email');
+    var Catagory = formData.get('catagory');
+    var URL = formData.get('URL');
+    console.log("name is ", Name);
+
+    validate(Name, number, Email, Catagory, URL); // Client side form validation 
+
+    var sendData = {
+        Name,
+        number,
+        Email,
+        Catagory,
+        URL
+    }
+    console.log(sendData);
+
+    fetch(API_URL + '/contacts', {
+            method: 'POST',
+            mode: 'cors',
+            body: JSON.stringify(sendData),
+            headers: {
+                'content-type': 'application/json'
+            }
+        })
+        .then(response => console.log(response))
+        .then(data => console.log(data));
+});
+
+
+const validate = (name, number, email, catagory, URL) => {
+    // while (errorElem.lastChild)
+    //     errorElem.removeChild(errorElem.lastChild);
+
+    if (!name && !number && !email && !catagory) {
+        CreateMessageDiv("Do not leave fields empty");
+    } else {
+        CreateMessageDiv("Success");
+    }
+}
+
+const CreateMessageDiv = (message) => {
+    const div = document.createElement('div');
+    const header = document.createElement('h3');
+
+    header.textContent = message;
+    div.appendChild(header);
+    errorElem.appendChild(div);
+}
